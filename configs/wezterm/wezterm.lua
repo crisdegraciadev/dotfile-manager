692266ab383dad4a8d2ee7841ba24c35d13f38d5
Cristian de Gracia Nuero <cristian.degracia.contractor@bbva.com>
2024-11-28 12:13:14 +0100

    update wezterm

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,42 +1,102 @@
-local wezterm = require("wezterm")
-return {
-	-- color_scheme = 'termnial.sexy',
-	color_scheme = "Catppuccin Mocha",
-	enable_tab_bar = false,
-	font_size = 16.0,
-	font = wezterm.font("JetBrains Mono"),
-	-- macos_window_background_blur = 40,
-	macos_window_background_blur = 30,
+-- Pull in the wezterm API
+local wezterm = require "wezterm"
 
-	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
-	-- window_background_image_hsb = {
-	-- 	brightness = 0.01,
-	-- 	hue = 1.0,
-	-- 	saturation = 0.5,
-	-- },
-	-- window_background_opacity = 0.92,
-	window_background_opacity = 1.0,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
-	window_decorations = "RESIZE",
-	keys = {
-		{
-			key = "f",
-			mods = "CTRL",
-			action = wezterm.action.ToggleFullScreen,
-		},
-		{
-			key = "'",
-			mods = "CTRL",
-			action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
-		},
-	},
-	mouse_bindings = {
-		-- Ctrl-click will open the link under the mouse cursor
-		{
-			event = { Up = { streak = 1, button = "Left" } },
-			mods = "CTRL",
-			action = wezterm.action.OpenLinkAtMouseCursor,
-		},
-	},
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = "Catppuccin Mocha"
+
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = false
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = false
+
+config.font_size = 16.0
+config.font = wezterm.font("JetBrains Mono")
+
+config.macos_window_background_blur = 30
+
+config.window_background_opacity = 1.0
+config.window_decorations = "RESIZE"
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+config.keys = {
+  {
+    mods = "LEADER",
+    key = "c",
+    action = wezterm.action.SpawnTab "CurrentPaneDomain",
+  },
+  {
+    mods = "LEADER",
+    key = "x",
+    action = wezterm.action.CloseCurrentPane { confirm = true }
+  },
+  {
+    mods = "LEADER",
+    key = "b",
+    action = wezterm.action.ActivateTabRelative(-1)
+  },
+  {
+    mods = "LEADER",
+    key = "n",
+    action = wezterm.action.ActivateTabRelative(1)
+  },
+  {
+    mods = "LEADER",
+    key = "|",
+    action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
+  },
+  {
+    mods = "LEADER",
+    key = "-",
+    action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
+  },
+  {
+    mods = "LEADER",
+    key = "LeftArrow",
+    action = wezterm.action.ActivatePaneDirection "Left"
+  },
+  {
+    mods = "LEADER",
+    key = "DownArrow",
+    action = wezterm.action.ActivatePaneDirection "Down"
+  },
+  {
+    mods = "LEADER",
+    key = "UpArrow",
+    action = wezterm.action.ActivatePaneDirection "Up"
+  },
+  {
+    mods = "LEADER",
+    key = "RightArrow",
+    action = wezterm.action.ActivatePaneDirection "Right"
+  },
 }
+
+for i = 1, 9 do
+  -- leader + number to activate that tab
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
+
+
+config.mouse_bindings = {
+  -- Ctrl-click will open the link under the mouse cursor
+  {
+    event = { Up = { streak = 1, button = "Left" } },
+    mods = "CTRL",
+    action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+}
+
+
+return config
